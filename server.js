b3579b22da54ba45d4b1b0173c0d16ae83d46c1b
fffiloni <sylvain.fillion15@gmail.com>
2018-01-28 22:30:44 +0100

    Sending file to remote

---
+++ server.js
@@ -0,0 +1,43 @@
+console.log("Stripe Payment is running.");
+
+var express = require("express");
+var stripe = require("stripe")("sktest");
+var hbs = require("hbs");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res){
+  res.render('index', {
+  });
+});
+
+app.get('/paysuccess', function(req, res){
+  res.render('paysuccess', {
+  });
+});
+
+app.post('/charge', function(req, res){
+  var token = req.body.stripeToken;
+  var chargeAmount = req.body.chargeAmount;
+  var charge = stripe.charges.create({
+    amount: chargeAmount,
+    currency: "eur",
+    source: token
+  }, function(err, charge){
+    if(err && err.type === "StripeCardError"){
+      console.log("Your card was declined");
+    }
+  });
+  console.log("Your payment was successful.");
+  res.redirect('/paysuccess');
+});
+
+app.listen(3000, function(){
+  console.log("Stripe is Running");
+});
